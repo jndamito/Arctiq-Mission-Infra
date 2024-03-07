19068c8572a542afddbd061feca67877c862d7b1
jndamito <josiasndam22@gmail.com>
2024-03-06 23:34:14 -0500

    adding argo installation to the repo

---
+++ self-hosted-runner/argocd.sh
@@ -0,0 +1,8 @@
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.4.7/manifests/install.yaml
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+export ARGOCD_SERVER=`kubectl get svc argocd-server -n argocd -o json | jq --raw-output '.status.loadBalancer.ingress[0].hostname'`
+export ARGO_PWD=`kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d`
+
+#not necessary if we configure from the console
+argocd login $ARGOCD_SERVER --username admin --password $ARGO_PWD --insecure
