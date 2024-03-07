b2a23769cf61b0e7782cad6c0c09a673dd7bccc0
jndamito <josiasndam22@gmail.com>
2024-03-06 23:14:20 -0500

    commit new vpc

---
+++ arctiq-vpc/provider.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "new-arctiq-489"
+    key    = "vpc/terraform.tfstate"
+    region = "us-east-1"
+}
+}
