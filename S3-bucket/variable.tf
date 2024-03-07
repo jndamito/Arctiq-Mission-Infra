80a890966708002ff9778556394ad0be613ea05b
jndamito <josiasndam22@gmail.com>
2024-03-06 23:07:10 -0500

    commit s3 and vpc

---
+++ S3-bucket/variable.tf
@@ -0,0 +1,14 @@
+variable "env" {
+  type    = string
+  default = "Dev"
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "versioning" {
+  type    = string
+  default = "Enabled"
+}
